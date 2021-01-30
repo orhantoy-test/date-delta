c4f8480420e79e8e972291704bb6287f6a440e60
Priyanka <Priyanka.mydesire@gmail.com>
2021-01-30 19:21:32 +0100

    converted js tests into jest tests and removed duplicate code in leap year.

--- delta.test.js
+++ delta.test.js
@@ -1,5 +1,5 @@
 const { test, expect } = require("@jest/globals");
-const { daysBetween } = require("./delta");
+const { daysBetween, isLeapYear } = require("./delta");
 
 test("calculates days between dates in same month", () => {
   const from = { year: 2020, month: 10, day: 2 };
@@ -15,36 +15,69 @@ TODO: Convert the following blocks into individual Jest tests.
 {
   const from = { year: 2020, month: 1, day: 20 };
   const to = { year: 2020, month: 2, day: 3 };
-
   console.log(`days: ${daysBetween(from, to)} (should be 14)`);
 }
+*/
 
+test("Calculates days between two consecutive months", () => {
+  const from = { year: 2020, month: 1, day: 20 };
+  const to = { year: 2020, month: 2, day: 3 };
+  expect(daysBetween(from, to)).toBe(14);
+});
+
+/*
 {
   const from = { year: 2019, month: 12, day: 20 };
   const to = { year: 2020, month: 1, day: 2 };
 
   console.log(`days: ${daysBetween(from, to)} (should be 13)`);
 }
+*/
 
+test("Calculates days between two years", () => {
+  const from = { year: 2019, month: 12, day: 31 };
+  const to = { year: 2020, month: 1, day: 2 };
+  expect(daysBetween(from, to)).toBe(2);
+  // isLeapYear(2020);
+});
+
+/*
 {
   const from = { year: 2020, month: 4, day: 20 };
   const to = { year: 2020, month: 5, day: 2 };
 
   console.log(`days: ${daysBetween(from, to)} (should be 12)`);
 }
-
+*/
+test("Calculates days between two months April & May", () => {
+  const from = { year: 2020, month: 4, day: 20 };
+  const to = { year: 2020, month: 5, day: 2 };
+  expect(daysBetween(from, to)).toBe(12);
+});
+/*
 {
   const from = { year: 2019, month: 2, day: 25 };
   const to = { year: 2019, month: 3, day: 2 };
 
   console.log(`days: ${daysBetween(from, to)} (should be 5)`);
-}
+}*/
+test("Calculates days between non leap year's months Feb & Mar", () => {
+  const from = { year: 2019, month: 2, day: 25 };
+  const to = { year: 2019, month: 3, day: 2 };
+  expect(daysBetween(from, to)).toBe(5);
+});
 
-{
+// {
+//   const from = { year: 2020, month: 2, day: 25 };
+//   const to = { year: 2020, month: 3, day: 2 };
+
+//   console.log(`days: ${daysBetween(from, to)} (should be 6)`);
+//   console.log(`isLeapYear: ${isLeapYear(from)} (should be yes)`);
+// }
+
+test("Calculates days between leap year's months Feb & Mar", () => {
   const from = { year: 2020, month: 2, day: 25 };
   const to = { year: 2020, month: 3, day: 2 };
-
-  console.log(`days: ${daysBetween(from, to)} (should be 6)`);
-  console.log(`isLeapYear: ${isLeapYear(from)} (should be yes)`);
-}
-*/
+  expect(daysBetween(from, to)).toBe(6);
+  expect(isLeapYear(from.year)).toBe(true);
+});
